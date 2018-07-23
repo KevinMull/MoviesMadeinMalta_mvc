bef54f093869cd2aed86f423fb9b5ddbb28bb90e
Kevin Mullarkey <kevmull65@gmail.com>
2018-07-23 18:31:28 +0100

    Malta Movie map updates
    Malta Movie map updates

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,6 +29,16 @@ namespace MaltaMoviesMVCcore.Controllers
             return View();
         }
 
+        public IActionResult Moviemap()
+        {
+            
+
+            return View();
+        }
+
+
+
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
